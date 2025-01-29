867abdfa5dc7ccd55bf25981da08606797b7ba04
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-01-29 23:11:59 +0100

    doc: observe the not-connected signal

--- rtl/ibex_core.sv
+++ rtl/ibex_core.sv
@@ -886,7 +886,7 @@ module ibex_core import ibex_pkg::*; #(
 
     // pcs support
     .pcs_mret_o(pcs_mret_o),
-    .pcs_csr_restore_mret_id_o(pcs_csr_restore_mret_id),
+    .pcs_csr_restore_mret_id_o(pcs_csr_restore_mret_id),// FIXME: NOT CONNECTED
     .pcs_restore_done_i         (pcs_restore_done_i),
     .start_pcs_o(start_pcs_o),
     .pcs_acive_i,
